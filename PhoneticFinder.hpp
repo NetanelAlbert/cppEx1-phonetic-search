4132a3f32c61b8e472e2f6f1035c0edc7a343e60
NetanelAlbert <netal90@gmail.com>
2020-03-23 16:18:50 +0200

    ifndef

--- PhoneticFinder.hpp
+++ PhoneticFinder.hpp
@@ -1,4 +1,10 @@
-//#include <string>
+/**
+Author - Netanel albert
+Date - 2020-3
+*/
+#ifndef _PhoneticFinder_
+#define _PhoneticFinder_
+
 #include <iostream>
 
 using namespace std;
@@ -6,3 +12,5 @@ using namespace std;
 namespace phonetic{
     string find(string s, string word);
 };
+
+#endif
